9e7d31b5d3c241e8b0071dcb5d2c60e59ef6599e
Ankit <40377554+ahir-ankit93@users.noreply.github.com>
2018-08-04 02:20:36 +0530

    add user update and delete
    curd operation using angular 
    bootstrap install while used 
    adduser on dumy api as well remove other 
    two way data binding demo

---
+++ app.module.ts
@@ -0,0 +1,23 @@
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { AddusersComponent } from './component/addusers/addusers.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddusersComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
